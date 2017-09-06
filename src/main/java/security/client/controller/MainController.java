7c58e5f8e528c7e5c37b76a29876321fabd9b832
Michael Kutsenko <m.kutsenko.work@gmail.com>
2017-09-06 11:22:10 +0300

    clean up

--- src/main/java/security/client/controller/MainController.java
+++ src/main/java/security/client/controller/MainController.java
@@ -3,17 +3,11 @@ package security.client.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
-import security.client.domain.User;
-import security.client.dto.VerificationRequestDto;
-
-import javax.validation.Valid;
 
 /**
  * Created by Mocart on 04-Sep-17.
@@ -34,32 +28,10 @@ public class MainController {
         this.restTemplate = restTemplate;
     }
 
-    @GetMapping(value = "/hello")
-    public ResponseEntity sayHello() {
-        return new ResponseEntity("hello there", HttpStatus.OK);
-    }
-
     @GetMapping(value = "/login")
     public String getLoginFrom() {
         return "login";
     }
-
-//    @PostMapping(value = "/login")
-//    public String getLoginFrom(@Valid VerificationRequestDto loginForm, BindingResult bindingResult, Model model) {
-//        if (bindingResult.hasErrors()) {
-//            model.addAttribute("error", "Please, fill all fields");
-//            model.addAttribute("loginFormDto", new VerificationRequestDto());
-//            return "login";
-//        }
-//
-//        try {
-//            getUser(loginForm);
-//        } catch (Exception e) {
-//            return "login";
-//        }
-//
-//        return "login";
-//    }
 
     @GetMapping(value = "/user/hello")
     public ResponseEntity helloUser() {
@@ -77,30 +49,4 @@ public class MainController {
         return new ResponseEntity("hello, admin", HttpStatus.OK);
     }
 
-
-//    private User getUser(VerificationRequestDto verificationRequestDto) throws Exception {
-//        System.out.println("hello");
-//
-//        verificationRequestDto.setId(ID);
-//        verificationRequestDto.setKey(KEY);
-//
-//        System.out.println(verificationRequestDto);
-//
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
-//
-//        HttpEntity<VerificationRequestDto> httpEntity = new HttpEntity<>(verificationRequestDto, headers);
-//        System.out.println(httpEntity.getBody());
-//
-//        ResponseEntity<User> response = restTemplate.postForEntity(URL_OF_VERIFICATION, httpEntity, User.class);
-//        logger.debug("Verification request has been sent. Status code of request: " + response.getStatusCode());
-//
-//        if (response.getStatusCode().equals(HttpStatus.OK))  {
-//            return response.getBody();
-//        }
-//        else {
-//            logger.error("An exception occurred. Status code: " + response.getStatusCodeValue());
-//            throw new Exception("Error. Status code: " + response.getStatusCodeValue());
-//        }
-//    }
 }
